fd7d6e2485877eb96be78b20e1c9e9266e0d2080
Gabriel Marques <gabriel.mjhu@gmail.com>
2024-01-04 14:11:16 -0300

    atualização do composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gabri/moneys_api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8e4f05f599d8bceadfbb6ba8c3a6dea2bf76604e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gabri/moneys_api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8e4f05f599d8bceadfbb6ba8c3a6dea2bf76604e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
